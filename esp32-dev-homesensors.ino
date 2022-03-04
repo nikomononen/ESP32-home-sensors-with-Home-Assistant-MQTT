9dc2e922a17b5bc83c06cda887eec3a634ea59da
Niko Mononen <niko.mononen@iltavarjo.net>
2022-03-04 17:17:24 +0200

    Add BME280 sensor

--- esp32-dev-homesensors.ino
+++ esp32-dev-homesensors.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
+#include <Adafruit_BME280.h>
 #include "SGP30.h"
 #include <DHT.h>
 #include <Arduino_JSON.h>
@@ -20,10 +21,10 @@ struct TSL2561Data {
   uint16_t luminosity;
   uint16_t broadband;
   uint16_t infrared;
-  uint16_t uvIndex;
+  int uvIndex;
 };
 
-struct SPG30Data {
+struct SGP30Data {
   float tvoc;
   uint16_t tvoc_base;
   float co2;
@@ -32,16 +33,25 @@ struct SPG30Data {
   float ethanol;
 };
 
+struct BME280Data {
+  float temperature;
+  float humidity;
+  float preassure;
+  float approximateAltitude;
+};
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 // DHT 22 temperature and humidity sensor
-#define DHTTYPE DHT22   // DHT 11 sensor 
+#define DHTTYPE DHT22
 int DHTPIN = 4;
 
 // UV sensor
 int UVPIN = 33;
 
 // MQTT Broker IP address
-IPAddress mqtt_server(0, 0, 0, 0);
-const char* MQTT_UID = "<MQTT-UID>";
+IPAddress mqtt_server(127, 0, 0, 1);
+const char* MQTT_UID = "<MQTT_UID>";
 
 // Wifi
 const char* ssid = "<ssid>";
@@ -93,11 +103,16 @@ DHT dht(DHTPIN, DHTTYPE);
 // The address will be different depending on whether you leave
 // the ADDR pin float (addr 0x39), or tie it to ground or vcc. In those cases
 // use TSL2561_ADDR_LOW (0x29) or TSL2561_ADDR_HIGH (0x49) respectively
+
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, TSL2561_UNIQUE_ID);
+
+#define BME289_ADDR_FLOAT 0x76
+Adafruit_BME280 bme;
 
 // Device setup, run once
 void setup(void) {
   Serial.begin(115200);
+  while ( !Serial ) delay(100);   // wait for native usb
   
   // Start DHT22 sensor
   dht.begin();
@@ -149,24 +164,27 @@ void setup(void) {
   
   // Configure SGP32 sensor
   configure_sgp30_sensor();
+
+  // Configure bme280 sensor
+  configure_bme280_sensor();
 }
 
 void register_MQTT_sensors_to_homeassistant(){
   Serial.println("MTQQ connected. Registering Home assistant sensors");
   // Create Home Assistant sensors
-  JSONVar json;
+  JSONVar json = new JSONVar();
   
   // Temperature
   json["unique_id"] = "esp32-dev-temperature";
   //json["device_class"] = "temperature";
-  json["name"] = "Humidity";
+  json["name"] = "Temperature";
   json["state_topic"] = "homeassistant/sensor/esp32dev/state";
-  json["unit_of_measurement"] = "%";
-  json["value_template"] = "{{ value_json.humidity}}";
+  json["unit_of_measurement"] = "°C";
+  json["value_template"] = "{{ value_json.temperature}}";
   String jsonString = JSON.stringify(json);
-  char plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  client.publish("homeassistant/sensor/esp32dev/Temperature/config", plain);
+  char temperature[jsonString.length()+1];
+  jsonString.toCharArray(temperature, jsonString.length()+1);
+  client.publish("homeassistant/sensor/esp32dev/Temperature/config", temperature);
   
   // Humidity
   json = new JSONVar();
@@ -177,10 +195,10 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "%";
   json["value_template"] = "{{ value_json.temperature}}";
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32dev/Humidity/config", plain);
+  char humidity[jsonString.length()+1];
+  jsonString.toCharArray(humidity, jsonString.length()+1);
+  Serial.println(humidity);
+  client.publish("homeassistant/sensor/esp32dev/Humidity/config", humidity);
   
   // Feels like
   json = new JSONVar();
@@ -191,10 +209,10 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "°C";
   json["value_template"] = "{{ value_json.heatIndex}}";  
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32dev/Heatindex/config", plain);   
+  char feelsLike[jsonString.length()+1];
+  jsonString.toCharArray(feelsLike, jsonString.length()+1);
+  Serial.println(feelsLike);
+  client.publish("homeassistant/sensor/esp32dev/Heatindex/config", feelsLike);   
   
   // Luminosity
   json = new JSONVar();
@@ -205,10 +223,10 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "lux";
   json["value_template"] = "{{ value_json.luminosity}}";
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32dev/Luminosity/config", plain);
+  char luminosity[jsonString.length()+1];
+  jsonString.toCharArray(luminosity, jsonString.length()+1);
+  Serial.println(luminosity);
+  client.publish("homeassistant/sensor/esp32dev/Luminosity/config", luminosity);
   
   // UV light
   json = new JSONVar();
@@ -218,10 +236,10 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "";
   json["value_template"] = "{{ value_json.uv}}";  
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32devUvIndex/config", plain);
+  char uv[jsonString.length()+1];
+  jsonString.toCharArray(uv, jsonString.length()+1);
+  Serial.println(uv);
+  client.publish("homeassistant/sensor/esp32devUvIndex/config", uv);
   Serial.println("Home assistant sensors registered.");
 
   // TVOC light
@@ -232,10 +250,10 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "";
   json["value_template"] = "{{ value_json.tvoc}}";  
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32devTVOC/config", plain);
+  char tvoc[jsonString.length()+1];
+  jsonString.toCharArray(tvoc, jsonString.length()+1);
+  Serial.println(tvoc);
+  client.publish("homeassistant/sensor/esp32devTVOC/config", tvoc);
 
   // CO2
   json = new JSONVar();
@@ -245,10 +263,10 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "";
   json["value_template"] = "{{ value_json.co2}}";  
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32devCO2/config", plain);
+  char co2[jsonString.length()+1];
+  jsonString.toCharArray(co2, jsonString.length()+1);
+  Serial.println(co2);
+  client.publish("homeassistant/sensor/esp32devCO2/config", co2);
   
   // H2
   json = new JSONVar();
@@ -258,10 +276,10 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "";
   json["value_template"] = "{{ value_json.h2}}";  
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32devH2/config", plain);
+  char h2[jsonString.length()+1];
+  jsonString.toCharArray(h2, jsonString.length()+1);
+  Serial.println(h2);
+  client.publish("homeassistant/sensor/esp32devH2/config", h2);
   
   // Ethanol
   json = new JSONVar();
@@ -271,10 +289,66 @@ void register_MQTT_sensors_to_homeassistant(){
   json["unit_of_measurement"] = "";
   json["value_template"] = "{{ value_json.ethanol}}";  
   jsonString = JSON.stringify(json);
-  plain[jsonString.length()+1];
-  jsonString.toCharArray(plain, jsonString.length()+1);
-  Serial.println(plain);
-  client.publish("homeassistant/sensor/esp32devEthanol/config", plain);
+  char ethanol[jsonString.length()+1];
+  jsonString.toCharArray(ethanol, jsonString.length()+1);
+  Serial.println(ethanol);
+  client.publish("homeassistant/sensor/esp32devEthanol/config", ethanol);
+
+  // BME280 Temperature
+  json = new JSONVar();
+  json["unique_id"] = "esp32-dev-bme280-temperature";
+  //json["device_class"] = "temperature";
+  json["name"] = "BME280 Temperature";
+  json["state_topic"] = "homeassistant/sensor/esp32dev/state";
+  json["unit_of_measurement"] = "°C";
+  json["value_template"] = "{{ value_json.bme280_temperature}}";
+  jsonString = JSON.stringify(json);
+  char bme280temperature[jsonString.length()+1];
+  jsonString.toCharArray(bme280temperature, jsonString.length()+1);
+  Serial.println(bme280temperature);
+  client.publish("homeassistant/sensor/esp32dev/BME280-Temperature/config", bme280temperature);
+ 
+  // BME280 Humidity
+  json = new JSONVar();
+  json["unique_id"] = "esp32-dev-bme280-humidity";
+  //json["device_class"] = "temperature";
+  json["name"] = "BME280 Humidity";
+  json["state_topic"] = "homeassistant/sensor/esp32dev/state";
+  json["unit_of_measurement"] = "%";
+  json["value_template"] = "{{ value_json.bme280_humidity}}";
+  jsonString = JSON.stringify(json);
+  char bme280humidity[jsonString.length()+1];
+  jsonString.toCharArray(bme280humidity, jsonString.length()+1);
+  Serial.println(bme280humidity);
+  client.publish("homeassistant/sensor/esp32dev/BME280-Humidity/config", bme280humidity);
+  
+  // BME280 Preassure
+  json = new JSONVar();
+  json["unique_id"] = "esp32-dev-bme280-preassure";
+  //json["device_class"] = "temperature";
+  json["name"] = "BME280 Preassure";
+  json["state_topic"] = "homeassistant/sensor/esp32dev/state";
+  json["unit_of_measurement"] = "hPa";
+  json["value_template"] = "{{ value_json.bme280_preassure}}";
+  jsonString = JSON.stringify(json);
+  char bme280preassure[jsonString.length()+1];
+  jsonString.toCharArray(bme280preassure, jsonString.length()+1);
+  Serial.println(bme280preassure);
+  client.publish("homeassistant/sensor/esp32dev/BME280-Preassure/config", bme280preassure);
+
+  // BME280 Approximate altitude
+  json = new JSONVar();
+  json["unique_id"] = "esp32-dev-bme280-altitude";
+  //json["device_class"] = "temperature";
+  json["name"] = "BME280 Altitude";
+  json["state_topic"] = "homeassistant/sensor/esp32dev/state";
+  json["unit_of_measurement"] = "hPa";
+  json["value_template"] = "{{ value_json.bme280_altitude}}";
+  jsonString = JSON.stringify(json);
+  char bme280altitude[jsonString.length()+1];
+  jsonString.toCharArray(bme280altitude, jsonString.length()+1);
+  Serial.println(bme280altitude);
+  client.publish("homeassistant/sensor/esp32dev/BME280-Altitude/config", bme280altitude);
   
   Serial.println("Home assistant sensors registered.");
 }
@@ -319,6 +393,15 @@ void configure_tls25621_sensor(void)
   }
 }
 
+void configure_bme280_sensor() {
+  //status = bme.begin(bme280_ADDRESS_ALT, bme280_CHIPID);
+  unsigned status;
+  status = bme.begin(BME289_ADDR_FLOAT);
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  }
+}
+
 void configure_sgp30_sensor(){
   Serial.println(SGP30_LIB_VERSION);
   Serial.println();
@@ -352,6 +435,7 @@ void configure_sgp30_sensor(){
 void handleRoot() {
   server.send(200, "text/plain", "hello from esp8266!");
 }
+
 void handleDHT22() {
     DHT22Data dht_state = read_dht();
     // Create json object
@@ -432,7 +516,7 @@ DHT22Data read_dht() {
   return data;
 }
 
-SPG30Data read_spg30() {
+SGP30Data read_sgp30() {
   // First 10-20 readings will always be TVOC 0 ppb eCO2 400 ppm. 
   SGP.measure(true);  
   
@@ -445,7 +529,17 @@ SPG30Data read_spg30() {
   float h2 = SGP.getH2();
   float ethanol = SGP.getEthanol();
   
-  SPG30Data data = {tvoc, tvoc_base, co2, co2_base, h2, ethanol};
+  SGP30Data data = {tvoc, tvoc_base, co2, co2_base, h2, ethanol};
+  return data;
+}
+
+BME280Data read_bme280() {
+  float preassure = bme.readPressure() / 100.0F;
+  float humidity = bme.readHumidity();
+  float temperature = bme.readTemperature();
+  float approximateAltitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  
+  BME280Data data = {temperature, humidity, preassure, approximateAltitude};
   return data;
 }
 
@@ -462,9 +556,12 @@ TSL2561Data read_luminosity(){
  
   // Populate broadband and infrared with the latest values 
   tsl.getLuminosity (&broadband, &infrared);
-  // UV
-  int uvVal = digitalRead(UVPIN);
   
+  // UV
+  uint16_t sensorValue = analogRead(UVPIN);
+  uint16_t sensorVoltage = (sensorValue / 4095) * 3.3;
+  uint16_t uvVal = getUvIndex(sensorVoltage);
+
   TSL2561Data data = {event.light, broadband, infrared, uvVal};
   return data;
 }
@@ -476,6 +573,40 @@ const char* stringToCharArray(String message){
   char plain[message.length()];
   message.toCharArray(plain, message.length());
   return plain;
+}
+
+uint16_t getUvIndex(uint16_t sensorValue){
+  uint16_t index = 0;
+  
+  // Voltage in miliVolts
+  uint16_t voltage = (sensorValue * (5.0 / 1023.0)) * 1000;  
+  
+  if (voltage >= 0 && voltage < 227) {
+    index = 0;
+  } else if (voltage >= 227 && voltage < 318) {
+    index = 1;
+  } else if (voltage >= 318 && voltage < 408) {
+    index = 2;
+  } else if (voltage >= 408 && voltage < 503) {
+    index = 3;
+  } else if (voltage >= 503 && voltage < 606) {
+    index = 4;
+  } else if (voltage >= 606 && voltage < 696) {
+    index = 5;
+  } else if (voltage >= 696 && voltage < 795) {
+    index = 6;
+  } else if (voltage >= 795 && voltage < 881) {
+    index = 7;
+  } else if (voltage >= 881 && voltage < 976) {
+    index = 8;
+  } else if (voltage >= 976 && voltage < 1079) {
+    index = 9;
+  } else if (voltage >= 1079 && voltage < 1170) {
+    index = 10;
+  } else {
+    index = 11;
+  }
+  return index;
 }
 
 // Last time bublished to MQTT
@@ -514,8 +645,9 @@ void loop(void) {
     // Read sensors
     DHT22Data temperature = read_dht();
     TSL2561Data luminosity = read_luminosity();
-    SPG30Data spg30 = read_spg30();
-    
+    SGP30Data sgp30 = read_sgp30();
+    BME280Data bme280 = read_bme280();
+
     // Set up MTQQ
     if( client.connect(MQTT_UID)) {
       Serial.println("MTQQ connected");
@@ -533,7 +665,7 @@ void loop(void) {
       json["humidity"] = temperature.humidity;
       
       // Heat index - Temperature with humidity combined
-      json["heatIndex"] = round(temperature.heatIndex);
+      json["heatIndex"] = temperature.heatIndex;
       
       // Luminosity
       json["luminosity"] = luminosity.luminosity;
@@ -542,18 +674,30 @@ void loop(void) {
       json["uv"] = luminosity.uvIndex;
 
       // TVOC
-      json["tvoc"] = spg30.tvoc;
-      json["tvocBase"] = spg30.tvoc_base;
+      json["tvoc"] = sgp30.tvoc;
+      json["tvocBase"] = sgp30.tvoc_base;
       
       // CO2
-      json["co2"] = spg30.co2;
-      json["co2Base"] = spg30.co2_base;
+      json["co2"] = sgp30.co2;
+      json["co2Base"] = sgp30.co2_base;
 
       // H2
-      json["h2"] = spg30.h2;
+      json["h2"] = sgp30.h2;
       
       // Ethanol
-      json["ethanol"] = spg30.ethanol;
+      json["ethanol"] = sgp30.ethanol;
+
+      // BME280 temperature
+      json["bme280_temperature"] = bme280.temperature;
+
+      // BME280 humidity
+      json["bme280_humidity"] = bme280.humidity;
+
+      // BME280 preassure
+      json["bme280_preassure"] = bme280.preassure;
+
+      // BME280 altitudr
+      json["bme280_altitude"] = bme280.approximateAltitude;
       
       // Return JSON
       String jsonString = JSON.stringify(json);
